51f7b97f6b61999964e4c63753d34613109d644f
Mingxuan Wang <reekystive@gmail.com>
2024-02-23 02:26:11 +0800

    feat: update floating action button

--- app/src/main/java/org/reeky/natutils/ui/component/AddFAB.kt
+++ app/src/main/java/org/reeky/natutils/ui/component/AddFAB.kt
@@ -2,15 +2,16 @@ package org.reeky.natutils.ui.component
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun AddFAB(onPress: () -> Unit) {
-    FloatingActionButton(
-        onClick = { onPress() }
-    ) {
-        Icon(Icons.Default.Add, contentDescription = "Add")
-    }
+    ExtendedFloatingActionButton(
+        onClick = { onPress() },
+        icon = { Icon(Icons.Default.Add, "Add") },
+        text = { Text(text = "Add") },
+    )
 }
