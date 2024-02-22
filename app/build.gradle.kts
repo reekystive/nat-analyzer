03b3ac192bf91f26f5add2435256526b1d57209e
Mingxuan Wang <reekystive@gmail.com>
2024-02-23 01:54:34 +0800

    chore: add accompanist dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.android.material)
+    implementation(libs.accompanist)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
